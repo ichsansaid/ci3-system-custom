9cc3245549c7b96499d82b06131284dd8ceccd99
ichsansaid <ichsann.saidd@gmail.com>
2021-01-04 12:40:45 +0700

    Foreign Key Validation

--- core/Model.php
+++ core/Model.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CodeIgniter
  *
@@ -35,7 +36,7 @@
  * @since	Version 1.0.0
  * @filesource
  */
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
  * Model Class
@@ -47,12 +48,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @link		https://codeigniter.com/user_guide/libraries/config.html
  */
 
-function customError($errno, $errstr) {
+function customError($errno, $errstr)
+{
 	echo "<b>Error:</b> [$errno] $errstr";
 	die();
 }
 
-class CI_Model {
+class CI_Model
+{
 
 	/**
 	 * Class constructor
@@ -60,7 +63,7 @@ class CI_Model {
 	 * @link	https://github.com/bcit-ci/CodeIgniter/issues/5332
 	 * @return	void
 	 */
-	
+
 	private $data_model = [];
 	private $data_clean;
 	private $form_error = [];
@@ -73,136 +76,165 @@ class CI_Model {
 	protected $main_table;
 	protected $import_models = [];
 	protected $selected_form;
+	protected $__primary_key;
 
-	public function __construct() {
-		set_error_handler("customError");
-		foreach($this->import_models as $key=>$value){
-			if(is_string($key)){
-				if(!isset(get_instance()->$value)){
+	public function __construct()
+	{
+		foreach ($this->import_models as $key => $value) {
+			if (is_string($key)) {
+				if (!isset(get_instance()->$value)) {
 					get_instance()->load->model($key, $value);
 				}
 			} else {
-				if(!isset(get_instance()->$value)){
+				if (!isset(get_instance()->$value)) {
 					get_instance()->load->model($value);
 				}
 			}
 		}
 		$this->pre_load();
 		$this->form = $this->form();
+		foreach($this->form as $id=>$value){
+			if (!isset($this->form[$id]['__table']))
+				$this->form[$id]['__table'] = $this->__table;
+		}
 	}
 
-	public function table($form){
-		return $this->form[$id]['__table'];
+	public function table($form)
+	{
+		return $this->form[$form]['__table'];
 	}
 
-	protected function pre_load(){
+	protected function pre_load()
+	{
 		return;
 	}
 
-	protected function form(){
+	protected function form()
+	{
 		return null;
 	}
 
-	public function get_form($id=null){
-		if($id !== null){
-			if(count($this->form) == 0){
+	public function get_form($id = null)
+	{
+		if ($id !== null) {
+			if (count($this->form) == 0) {
 				$this->form = $this->form();
 			}
-			if(!isset($this->form[$id])){
-				show_error("Form ".$id." not exist");
+			if (!isset($this->form[$id])) {
+				show_error("Form " . $id . " not exist");
 			}
-			if(!isset($this->form[$id]['__table']))
+			if (!isset($this->form[$id]['__table']))
 				$this->form[$id]['__table'] = $this->__table;
 			return $this->form[$id];
 		} else {
 			return $this->form;
 		}
-		
 	}
 
-	public function get_data_model(){
+	public function get_data_model()
+	{
 		return $this->data_model;
 	}
 
-	public function data($data = -1){
-		if($data == -1){
+	public function data($data = -1)
+	{
+		if ($data == -1) {
 			return $this->data_model;
 		}
 		$this->data_model = $data;
 		return $this;
 	}
 
-	public function data_clean($form = null){
-		if($form == '__all__'){
+	public function data_clean($form = null)
+	{
+		if ($form == '__all__') {
 			$return = [];
-			foreach($this->data_clean as $key=>$value){
+			foreach ($this->data_clean as $key => $value) {
 				$return = array_merge($return, $value);
 			}
 			return $return;
 		}
-		if($form == null){
-			if(count($this->form_use) > 0){
+		if ($form == null) {
+			if (count($this->form_use) > 0) {
 				return $this->data_clean[$this->form_use[count($this->form_use) - 1]];
 			} else {
 				return [];
 			}
 		}
-		if(isset($this->data_clean[$form])){
+		if (isset($this->data_clean[$form])) {
 			return $this->data_clean[$form];
 		}
 		return [];
 	}
 
-	public function form_error(){
+	public function form_error()
+	{
 		return $this->form_error;
 	}
 
-	public function use_form($id){
-		if(!is_array($id)){
+	public function use_form($id)
+	{
+		if (!is_array($id)) {
 			$ganti = [$id];
 		} else {
 			$ganti = $id;
 		}
-		foreach($ganti as $val){
-			if(!isset($this->form[$val])){
+		foreach ($ganti as $val) {
+			if (!isset($this->form[$val])) {
 				$sss = false;
-				foreach($this->import_models as $key=>$value){
-					if(isset(get_instance()->$value)){
-						if(isset(get_instance()->$value->get_form()[$val])){
-							$this->form[$val]=get_instance()->$value->get_form()[$val];
-							$sss=true;
+				foreach ($this->import_models as $key => $value) {
+					if (isset(get_instance()->$value)) {
+						if (isset(get_instance()->$value->get_form()[$val])) {
+							$this->form[$val] = get_instance()->$value->get_form()[$val];
+							$sss = true;
 							break;
 						}
 					}
 				}
-				if($sss === false) show_error("Form ".$id." not exist");
+				if ($sss === false) show_error("Form " . $val . " not exist");
 			}
 		}
 		$this->form_use = $ganti;
 		return $this;
 	}
 
-	public function select_form($id){
+	public function select_form($id)
+	{
 		$this->selected_form = $id;
 		return $this;
 	}
 
-	public function valid(){
+	public function valid()
+	{
 		$ids = $this->form_use;
-		if(!is_array($ids)){
+		if (!is_array($ids)) {
 			$ids = [$ids];
 		}
-		foreach($ids as $id){
-			if(isset($this->form[$id])){
+		foreach ($ids as $id) {
+			if (isset($this->form[$id])) {
 				$this->form_validation->set_data($this->data_model);
-				foreach($this->form[$id] as $key=>$value){
-					if($key != '__table'){
-						foreach($value[1] as $key2=>$value2){
-							if($key2 === '__create' || $key2 === '__update' || $key2 === '__filter'){
+				foreach ($this->form[$id] as $key => $value) {
+					if ($key != '__table') {
+						foreach ($value[1] as $key2 => $value2) {
+							if ($key2 === '__create' || $key2 === '__update' || $key2 === '__filter') {
 								unset($value[1][$key2]);
+							} else {
+								$len = strlen('!!');
+								if((substr($key2, 0, $len) === '!!')){
+									$key2 = str_replace("!!", "", $key2);
+									if(method_exists($this->validation, $key2)){
+										$this->validation->setModel($value2)->setLang($this->form_lang)->setValidation($key2);
+										array_push($value[1], [$key2, function($asd){
+											return $this->validation->run($asd);
+										}]);
+										unset($value[1]["!!".$key2]);
+									} else {
+										show_error("Validation $value2 not valid");
+									}
+								}
 							}
 						}
-						if(count($this->form_lang) > 1){
+						if (count($this->form_lang) > 1) {
 							$this->form_validation->set_rules($key, $value[0], isset($value[1]) ? $value[1] : [], $this->form_lang);
 						} else {
 							$this->form_validation->set_rules($key, $value[0], isset($value[1]) ? $value[1] : []);
@@ -211,12 +243,13 @@ class CI_Model {
 				}
 			}
 		}
-		if($this->form_validation->run() == true){
-			foreach($this->form as $key=>$value){
-				if($key != '__table'){
-					if(is_array($value)){
-						foreach($value as $key2=>$value2){
-							if(isset($this->data_model[$key2])){
+
+		if ($this->form_validation->run() == true) {
+			foreach ($this->form as $key => $value) {
+				if ($key != '__table') {
+					if (is_array($value)) {
+						foreach ($value as $key2 => $value2) {
+							if (isset($this->data_model[$key2])) {
 								$this->data_clean[$key][$key2] = $this->data_model[$key2];
 							}
 						}
@@ -229,37 +262,48 @@ class CI_Model {
 		return count($this->form_error) <= 0;
 	}
 
-	public function get_latest_form(){
-		if(isset($this->form[$this->form_use[count($this->form_use) - 1]])) {
+	public function get_latest_form()
+	{
+		if (isset($this->form[$this->form_use[count($this->form_use) - 1]])) {
 			return $this->form_use[count($this->form_use) - 1];
 		}
 	}
 
-	public function get_table_form($form){
+	public function get_object(){
+		return $this;
+	}
+
+	public function get_table_form($form)
+	{
 		$table = "";
-		if($form == null){
+		if ($form == null) {
 			$form = $this->get_latest_form();
 		}
 		$form = $this->form[$form];
-		if(isset($form['__table'])){
+		if (isset($form['__table'])) {
 			$table = $form['__table'];
-		} else if($this->__table !== null){
+		} else if ($this->__table !== null) {
 			$table = $this->__table;
 		}
-		if($table == ''){
+		if ($table == '') {
 			show_error('Kamu harus menambah __table pada Form / Model');
 		}
 		return $table;
 	}
 
 	public function __toString()
-    {
-        return $this->__table;
+	{
+		return $this->__table;
 	}
 
-	public function datatable($form, $type='get')
+	public function datatable($form, $type = 'get')
 	{
-		if($type == 'get'){
+		$draw = "";
+		$start = "";
+		$length = "";
+		$order = "";
+		$search = "";
+		if ($type == 'get') {
 			$draw = intval($this->input->get("draw"));
 			$start = intval($this->input->get("start"));
 			$length = intval($this->input->get("length"));
@@ -272,19 +316,19 @@ class CI_Model {
 			$order = $this->input->post("order");
 			$search = $this->input->post("search");
 		}
-		
+
 		$search = $search['value'];
 		$col = 0;
 		$dir = "";
 		$fields = [];
-		foreach($this->get_form($form) as $key=>$value){
-			if($key !== '__table'){
+		foreach ($this->get_form($form) as $key => $value) {
+			if ($key !== '__table') {
 				array_push($fields, $key);
 			}
 		}
-		$sel = implode($fields, ",");
-		$this->db->select($sel)->from($this->__table);
-		$qr = $this->db->_compile_select(); 
+		$sel = implode(",", $fields);
+		$this->db->select($sel)->from($this->get_form($form)["__table"]);
+		$qr = $this->db->_compile_select();
 		if (!empty($order)) {
 			foreach ($order as $o) {
 				$col = $o['column'];
@@ -312,14 +356,7 @@ class CI_Model {
 		}
 		$this->db->limit($length, $start);
 		$results = $this->db->query($this->db->_compile_select());
-		$data = array();
-		foreach ($results->result_array() as $rows) {
-			$arr = [];
-			foreach($rows as $value){
-				$arr[] = $value;
-			}
-			$data[] = $arr;
-		}
+		$data = $results->result_array();
 		$total_employees = $this->db->query($qr)->num_rows();
 		$output = array(
 			"draw" => $draw,
@@ -331,26 +368,27 @@ class CI_Model {
 	}
 
 
-	public function create($form = null, $additional_data=[]){
-		if($form == null){
-			if($this->selected_form != null){
+	public function create($form = null, $additional_data = [])
+	{
+		if ($form == null) {
+			if ($this->selected_form != null) {
 				$form = $this->selected_form;
 			} else {
 				$form = $this->get_latest_form();
 			}
 		}
 		$table = $this->get_table_form($form);
-		foreach($this->form[$form] as $key2=>$value2){
-			if(is_array($value2)){
-				foreach($value2 as $key3=>$value3){
-					if(is_array($value3)){
-						foreach($value3 as $key4=>$value4){
-							if($key4 === '__filter' || $key4 === '__create'){
+		foreach ($this->form[$form] as $key2 => $value2) {
+			if (is_array($value2)) {
+				foreach ($value2 as $key3 => $value3) {
+					if (is_array($value3)) {
+						foreach ($value3 as $key4 => $value4) {
+							if ($key4 === '__filter' || $key4 === '__create') {
 								$ret = $value4($key2, $this->data_clean, $this);
-								if(!is_array($ret)){
+								if (!is_array($ret)) {
 									$ret = [$ret];
 								}
-								if(count($ret) > 1){
+								if (count($ret) > 1) {
 									$this->data_clean[$form][$ret[1]] = $ret[0];
 									unset($this->data_clean[$form][$key2]);
 								} else {
@@ -367,9 +405,10 @@ class CI_Model {
 		$this->bind($id, $form);
 	}
 
-	public function delete($form = null){
-		if($form == null){
-			if($this->selected_form != null){
+	public function delete($form = null)
+	{
+		if ($form == null) {
+			if ($this->selected_form != null) {
 				$form = $this->selected_form;
 			}
 		}
@@ -377,27 +416,28 @@ class CI_Model {
 		$this->db_delete($table);
 	}
 
-	public function update($form = null){
-		if($form == null){
-			if($this->selected_form != null){
+	public function update($form = null)
+	{
+		if ($form == null) {
+			if ($this->selected_form != null) {
 				$form = $this->selected_form;
 			}
 		}
 		$table = $this->get_table_form($form);
-		if($form == null){
+		if ($form == null) {
 			$form = $this->form_use[count($this->form_use) - 1];
 		}
-		foreach($this->form[$form] as $key2=>$value2){
-			if(is_array($value2)){
-				foreach($value2 as $key3=>$value3){
-					if(is_array($value3)){
-						foreach($value3 as $key4=>$value4){
-							if($key4 === '__filter' || $key4 === '__update'){
+		foreach ($this->form[$form] as $key2 => $value2) {
+			if (is_array($value2)) {
+				foreach ($value2 as $key3 => $value3) {
+					if (is_array($value3)) {
+						foreach ($value3 as $key4 => $value4) {
+							if ($key4 === '__filter' || $key4 === '__update') {
 								$ret = $value4($key2, $this->data_clean, $this);
-								if(!is_array($ret)){
+								if (!is_array($ret)) {
 									$ret = [$ret];
-								}	
-								if(count($ret) > 1){
+								}
+								if (count($ret) > 1) {
 									$this->data_clean[$form][$ret[1]] = $ret[0];
 									unset($this->data_clean[$form][$key2]);
 								} else {
@@ -413,25 +453,28 @@ class CI_Model {
 		return true;
 	}
 
-	public function get($form){
+	public function get($form)
+	{
 		$table = $this->get_table_form($form);
 		return $this->db_get($table);
 	}
-	
 
-	public function db_get($table){
+
+	public function db_get($table)
+	{
 		return $this->db->get($table);
 	}
 
-	public function db_insert($data, $table){
+	public function db_insert($data, $table)
+	{
 		$this->db->insert($table, $data);
-		
 	}
 
-	public function db_update($data, $table){
-		if(!$this->data_bind == null){
-			if(isset($this->data_bind[$table])){
-				if(isset($this->data_bind[$table]['id'])){
+	public function db_update($data, $table)
+	{
+		if (!$this->data_bind == null) {
+			if (isset($this->data_bind[$table])) {
+				if (isset($this->data_bind[$table]['id'])) {
 					$this->db->where('id', $this->data_bind[$table]['id']);
 				} else {
 					show_error("ID Data Binding Error");
@@ -446,10 +489,11 @@ class CI_Model {
 		return true;
 	}
 
-	public function db_delete($table){
-		if(!$this->data_bind == null){
-			if(isset($this->data_bind[$table])){
-				if(isset($this->data_bind[$table]['id'])){
+	public function db_delete($table)
+	{
+		if (!$this->data_bind == null) {
+			if (isset($this->data_bind[$table])) {
+				if (isset($this->data_bind[$table]['id'])) {
 					$this->db->where('id', $this->data_bind[$table]['id']);
 				} else {
 					show_error("ID Data Binding Error");
@@ -463,27 +507,33 @@ class CI_Model {
 		return $this->db->delete($table);
 	}
 
-	public function data_bind($form){
+	public function data_bind($form)
+	{
 		$table = $this->get_table_form($form);
 		return $this->data_bind[$table];
 	}
 
-	public function bind($pk = null, $form = null){
-		if($pk !== null){
-			if(!is_array($pk)){
+	public function bind($pk = null, $form = null)
+	{
+		if ($pk !== null) {
+			if (is_array($pk)) {
 				$this->db->where($pk);
 			} else {
 				$this->db->where('id', $pk);
 			}
 		}
-		if($form == null){
-			if($this->selected_form != null){
+		if ($form == null) {
+			if ($this->selected_form != null) {
 				$form = $this->selected_form;
 			}
 		}
-		$table = $this->get_table_form($form);
+		if($form == null){
+			$table = $this->__table;
+		} else {
+			$table = $this->get_table_form($form);
+		}
 		$data_bind = $this->db_get($table)->result_array();
-		if(count($data_bind) > 0){
+		if (count($data_bind) > 0) {
 			$this->data_bind[$table] = $data_bind[0];
 		} else {
 			unset($this->data_bind[$table]);
@@ -491,11 +541,12 @@ class CI_Model {
 		return $this;
 	}
 
-	public function is_null(){
+	public function is_null()
+	{
 		return $this->data_bind == null;
 	}
-	 
-	
+
+
 
 	/**
 	 * __get magic
@@ -513,5 +564,4 @@ class CI_Model {
 		//	most likely a typo in your model code.
 		return get_instance()->$key;
 	}
-
 }
